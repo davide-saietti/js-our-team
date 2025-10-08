02c072d48b42f08cea3862dc6c9bc2254f305d84
davide-saietti <imbianchino.nando@gmail.com>
2025-10-08 18:45:28 +0200

    feat: team list foreach

---
+++ script.js
@@ -0,0 +1,63 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+const container = document.getElementById('team-grid')
+
+
+teamMembers.forEach((member=>{
+
+  const column = document.createElement('div')
+  column.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-')
+
+
+  column.innerHTML = 
+`  
+  <div class="card h-40 text-center white-sm mb-5" >
+      <img src="${member.img}" class="card-img-top" alt="${member.name}"/>
+      <div class="card-body">
+        <h5 class="card-title">${member.name}</h5>
+        <p class="card-text text-muted">${member.role}</p>
+        <p class="card-text text-muted">${member.email}</p>
+      </div>
+    </div>`
+
+
+ container.appendChild(column)
+
+}))
