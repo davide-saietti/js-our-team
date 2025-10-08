45d2cf712457b19318c144b231706b5e3dd6207c
davide-saietti <imbianchino.nando@gmail.com>
2025-10-08 18:53:52 +0200

    feat: team list cycle for variant

--- script.js
+++ script.js
@@ -61,3 +61,44 @@ teamMembers.forEach((member=>{
  container.appendChild(column)
 
 }))
+
+
+/* esercizio risolvibile con ciclo for.
+è possibile risolvere anche con un semplice ciclo for. al posto del foreach
+
+for(i = 0; i<=teamMembers.length; i++){
+
+const member = teamMembers[i]
+
+
+const column = document.createElement('div')
+  column.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-')
+
+
+  column.innerHTML = 
+`  
+  <div class="card h-40 text-center white-sm mb-5" >
+      <img src="${member.img}" class="card-img-top" alt="${member.name}"/>
+      <div class="card-body">
+        <h5 class="card-title">${member.name}</h5>
+        <p class="card-text text-muted">${member.role}</p>
+        <p class="card-text text-muted">${member.email}</p>
+      </div>
+    </div>`
+
+
+ container.appendChild(column)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+*/
